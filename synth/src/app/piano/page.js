c2cf4f3b56cc144bb4ba869c25bbfd56358f80a0
Brad Kerr <134312764+Bradpk@users.noreply.github.com>
2023-11-09 13:39:38 -0500

    keys change on hover

--- synth/src/app/piano/page.js
+++ synth/src/app/piano/page.js
@@ -10,7 +10,8 @@ function Synth() {
     const [decay, setDecay] = useState(0.1);
     const [sustain, setSustain] = useState(0.1);
     const [release, setRelease] = useState(0.1);
-    const [playingNote, setPlayingNote] = useState(null);
+    const [activeButtons, setActiveButtons] = useState([]);
+
 
     const synth = new Tone.Synth({
         oscillator: {
@@ -53,14 +54,18 @@ function Synth() {
     const handleMouseDown = (note) => {
         const now = Tone.now();
         synth.triggerAttackRelease(note, "4n", now);
-        setPlayingNote(note);
-    };
-    
-    const handleMouseEnter = (note, e) => {
+        setActiveButtons([note]);
+      
+        setTimeout(() => {
+          setActiveButtons([]);
+        }, 10);
+      };
+      
+      const handleMouseEnter = (note, e) => {
         if (e.buttons === 1) {
-            handleMouseDown(note);
+          handleMouseDown(note);
         }
-    };
+      };
     //-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
     return (
         <>
@@ -107,54 +112,54 @@ function Synth() {
             </div>
         </div>
             <div className={styles.piano}>
-                <button className={styles.key} onMouseDown={() => handleMouseDown("C2")} onMouseEnter={(e) => handleMouseEnter("C2", e)}></button>
-                <button className={styles.black} onMouseDown={() => handleMouseDown("C#2")} onMouseEnter={(e) => handleMouseEnter("C#2", e)}></button>
-                <button className={styles.key} onMouseDown={() => handleMouseDown("D2")} onMouseEnter={(e) => handleMouseEnter("D2", e)}></button>
-                <button className={styles.black} onMouseDown={() => handleMouseDown("D#2")} onMouseEnter={(e) => handleMouseEnter("D#2", e)}></button>
-                <button className={styles.key} onMouseDown={() => handleMouseDown("E2")} onMouseEnter={(e) => handleMouseEnter("E2", e)}></button>
-                <button className={styles.key} onMouseDown={() => handleMouseDown("F2")} onMouseEnter={(e) => handleMouseEnter("F2", e)}></button>
-                <button className={styles.black} onMouseDown={() => handleMouseDown("F#2")} onMouseEnter={(e) => handleMouseEnter("F#2", e)}></button>
-                <button className={styles.key} onMouseDown={() => handleMouseDown("G2")} onMouseEnter={(e) => handleMouseEnter("G2", e)}></button>
-                <button className={styles.black} onMouseDown={() => handleMouseDown("G#2")} onMouseEnter={(e) => handleMouseEnter("G#2", e)}></button>
-                <button className={styles.key} onMouseDown={() => handleMouseDown("A2")} onMouseEnter={(e) => handleMouseEnter("A2", e)}></button>
-                <button className={styles.black} onMouseDown={() => handleMouseDown("A#2")} onMouseEnter={(e) => handleMouseEnter("A#2", e)}></button>
-                <button className={styles.key} onMouseDown={() => handleMouseDown("B2")} onMouseEnter={(e) => handleMouseEnter("B2", e)}></button>
-                <button className={styles.key} onMouseDown={() => handleMouseDown("C3")} onMouseEnter={(e) => handleMouseEnter("C3", e)}></button>
-                <button className={styles.black} onMouseDown={() => handleMouseDown("C#3")} onMouseEnter={(e) => handleMouseEnter("C#3", e)}></button>
-                <button className={styles.key} onMouseDown={() => handleMouseDown("D3")} onMouseEnter={(e) => handleMouseEnter("D3", e)}></button>
-                <button className={styles.black} onMouseDown={() => handleMouseDown("D#3")} onMouseEnter={(e) => handleMouseEnter("D#3", e)}></button>
-                <button className={styles.key} onMouseDown={() => handleMouseDown("E3")} onMouseEnter={(e) => handleMouseEnter("E3", e)}></button>
-                <button className={styles.key} onMouseDown={() => handleMouseDown("F3")} onMouseEnter={(e) => handleMouseEnter("F3", e)}></button>
-                <button className={styles.black} onMouseDown={() => handleMouseDown("F#3")} onMouseEnter={(e) => handleMouseEnter("F#3", e)}></button>
-                <button className={styles.key} onMouseDown={() => handleMouseDown("G3")} onMouseEnter={(e) => handleMouseEnter("G3", e)}></button>
-                <button className={styles.black} onMouseDown={() => handleMouseDown("G#3")} onMouseEnter={(e) => handleMouseEnter("G#3", e)}></button>
-                <button className={styles.key} onMouseDown={() => handleMouseDown("A3")} onMouseEnter={(e) => handleMouseEnter("A3", e)}></button>
-                <button className={styles.black} onMouseDown={() => handleMouseDown("A#3")} onMouseEnter={(e) => handleMouseEnter("A#3", e)}></button>
-                <button className={styles.key} onMouseDown={() => handleMouseDown("B3")} onMouseEnter={(e) => handleMouseEnter("B3", e)}></button>
-                <button className={styles.key} onMouseDown={() => handleMouseDown("C4")} onMouseEnter={(e) => handleMouseEnter("C4", e)}></button>
-                <button className={styles.black} onMouseDown={() => handleMouseDown("C#4")} onMouseEnter={(e) => handleMouseEnter("C#4", e)}></button>
-                <button className={styles.key} onMouseDown={() => handleMouseDown("D4")} onMouseEnter={(e) => handleMouseEnter("D4", e)}></button>
-                <button className={styles.black} onMouseDown={() => handleMouseDown("D#4")} onMouseEnter={(e) => handleMouseEnter("D#4", e)}></button>
-                <button className={styles.key} onMouseDown={() => handleMouseDown("E4")} onMouseEnter={(e) => handleMouseEnter("E4", e)}></button>
-                <button className={styles.key} onMouseDown={() => handleMouseDown("F4")} onMouseEnter={(e) => handleMouseEnter("F4", e)}></button>
-                <button className={styles.black} onMouseDown={() => handleMouseDown("F#4")} onMouseEnter={(e) => handleMouseEnter("F#4", e)}></button>
-                <button className={styles.key} onMouseDown={() => handleMouseDown("G4")} onMouseEnter={(e) => handleMouseEnter("G4", e)}></button>
-                <button className={styles.black} onMouseDown={() => handleMouseDown("G#4")} onMouseEnter={(e) => handleMouseEnter("G#4", e)}></button>
-                <button className={styles.key} onMouseDown={() => handleMouseDown("A4")} onMouseEnter={(e) => handleMouseEnter("A4", e)}></button>
-                <button className={styles.black} onMouseDown={() => handleMouseDown("A#4")} onMouseEnter={(e) => handleMouseEnter("A#4", e)}></button>
-                <button className={styles.key} onMouseDown={() => handleMouseDown("B4")} onMouseEnter={(e) => handleMouseEnter("B4", e)}></button>
-                <button className={styles.key} onMouseDown={() => handleMouseDown("C5")} onMouseEnter={(e) => handleMouseEnter("C5", e)}></button>
-                <button className={styles.black} onMouseDown={() => handleMouseDown("C#5")} onMouseEnter={(e) => handleMouseEnter("C#5", e)}></button>
-                <button className={styles.key} onMouseDown={() => handleMouseDown("D5")} onMouseEnter={(e) => handleMouseEnter("D5", e)}></button>
-                <button className={styles.black} onMouseDown={() => handleMouseDown("D#5")} onMouseEnter={(e) => handleMouseEnter("D#5", e)}></button>
-                <button className={styles.key} onMouseDown={() => handleMouseDown("E5")} onMouseEnter={(e) => handleMouseEnter("E5", e)}></button>
-                <button className={styles.key} onMouseDown={() => handleMouseDown("F5")} onMouseEnter={(e) => handleMouseEnter("F5", e)}></button>
-                <button className={styles.black} onMouseDown={() => handleMouseDown("F#5")} onMouseEnter={(e) => handleMouseEnter("F#5", e)}></button>
-                <button className={styles.key} onMouseDown={() => handleMouseDown("G5")} onMouseEnter={(e) => handleMouseEnter("G5", e)}></button>
-                <button className={styles.black} onMouseDown={() => handleMouseDown("G#5")} onMouseEnter={(e) => handleMouseEnter("G#5", e)}></button>
-                <button className={styles.key} onMouseDown={() => handleMouseDown("A5")} onMouseEnter={(e) => handleMouseEnter("A5", e)}></button>
-                <button className={styles.black} onMouseDown={() => handleMouseDown("A#5")} onMouseEnter={(e) => handleMouseEnter("A#5", e)}></button>
-                <button className={styles.key} onMouseDown={() => handleMouseDown("B5")} onMouseEnter={(e) => handleMouseEnter("B5", e)}></button>
+                <button className={`${styles.key} ${activeButtons.includes("C2") ? styles.buttonActive : ""}`} onMouseDown={() => handleMouseDown("C2")} onMouseEnter={(e) => handleMouseEnter("C2", e)}></button>
+                <button className={`${styles.black} ${activeButtons.includes("C#2") ? styles.buttonActive : ""}`} onMouseDown={() => handleMouseDown("C#2")} onMouseEnter={(e) => handleMouseEnter("C#2", e)}></button>
+                <button className={`${styles.key} ${activeButtons.includes("D2") ? styles.buttonActive : ""}`} onMouseDown={() => handleMouseDown("D2")} onMouseEnter={(e) => handleMouseEnter("D2", e)}></button>
+                <button className={`${styles.black} ${activeButtons.includes("D#2") ? styles.buttonActive : ""}`} onMouseDown={() => handleMouseDown("D#2")} onMouseEnter={(e) => handleMouseEnter("D#2", e)}></button>
+                <button className={`${styles.key} ${activeButtons.includes("E2") ? styles.buttonActive : ""}`} onMouseDown={() => handleMouseDown("E2")} onMouseEnter={(e) => handleMouseEnter("E2", e)}></button>
+                <button className={`${styles.key} ${activeButtons.includes("F2") ? styles.buttonActive : ""}`} onMouseDown={() => handleMouseDown("F2")} onMouseEnter={(e) => handleMouseEnter("F2", e)}></button>
+                <button className={`${styles.black} ${activeButtons.includes("F#2") ? styles.buttonActive : ""}`} onMouseDown={() => handleMouseDown("F#2")} onMouseEnter={(e) => handleMouseEnter("F#2", e)}></button>
+                <button className={`${styles.key} ${activeButtons.includes("G2") ? styles.buttonActive : ""}`} onMouseDown={() => handleMouseDown("G2")} onMouseEnter={(e) => handleMouseEnter("G2", e)}></button>
+                <button className={`${styles.black} ${activeButtons.includes("G#2") ? styles.buttonActive : ""}`} onMouseDown={() => handleMouseDown("G#2")} onMouseEnter={(e) => handleMouseEnter("G#2", e)}></button>
+                <button className={`${styles.key} ${activeButtons.includes("A2") ? styles.buttonActive : ""}`} onMouseDown={() => handleMouseDown("A2")} onMouseEnter={(e) => handleMouseEnter("A2", e)}></button>
+                <button className={`${styles.black} ${activeButtons.includes("A#2") ? styles.buttonActive : ""}`} onMouseDown={() => handleMouseDown("A#2")} onMouseEnter={(e) => handleMouseEnter("A#2", e)}></button>
+                <button className={`${styles.key} ${activeButtons.includes("B2") ? styles.buttonActive : ""}`} onMouseDown={() => handleMouseDown("B2")} onMouseEnter={(e) => handleMouseEnter("B2", e)}></button>
+                <button className={`${styles.key} ${activeButtons.includes("C3") ? styles.buttonActive : ""}`} onMouseDown={() => handleMouseDown("C3")} onMouseEnter={(e) => handleMouseEnter("C3", e)}></button>
+                <button className={`${styles.black} ${activeButtons.includes("C#3") ? styles.buttonActive : ""}`} onMouseDown={() => handleMouseDown("C#3")} onMouseEnter={(e) => handleMouseEnter("C#3", e)}></button>
+                <button className={`${styles.key} ${activeButtons.includes("D3") ? styles.buttonActive : ""}`} onMouseDown={() => handleMouseDown("D3")} onMouseEnter={(e) => handleMouseEnter("D3", e)}></button>
+                <button className={`${styles.black} ${activeButtons.includes("D#3") ? styles.buttonActive : ""}`} onMouseDown={() => handleMouseDown("D#3")} onMouseEnter={(e) => handleMouseEnter("D#3", e)}></button>
+                <button className={`${styles.key} ${activeButtons.includes("E3") ? styles.buttonActive : ""}`} onMouseDown={() => handleMouseDown("E3")} onMouseEnter={(e) => handleMouseEnter("E3", e)}></button>
+                <button className={`${styles.key} ${activeButtons.includes("F3") ? styles.buttonActive : ""}`} onMouseDown={() => handleMouseDown("F3")} onMouseEnter={(e) => handleMouseEnter("F3", e)}></button>
+                <button className={`${styles.black} ${activeButtons.includes("F#3") ? styles.buttonActive : ""}`} onMouseDown={() => handleMouseDown("F#3")} onMouseEnter={(e) => handleMouseEnter("F#3", e)}></button>
+                <button className={`${styles.key} ${activeButtons.includes("G3") ? styles.buttonActive : ""}`} onMouseDown={() => handleMouseDown("G3")} onMouseEnter={(e) => handleMouseEnter("G3", e)}></button>
+                <button className={`${styles.black} ${activeButtons.includes("G#3") ? styles.buttonActive : ""}`} onMouseDown={() => handleMouseDown("G#3")} onMouseEnter={(e) => handleMouseEnter("G#3", e)}></button>
+                <button className={`${styles.key} ${activeButtons.includes("A3") ? styles.buttonActive : ""}`} onMouseDown={() => handleMouseDown("A3")} onMouseEnter={(e) => handleMouseEnter("A3", e)}></button>
+                <button className={`${styles.black} ${activeButtons.includes("A#3") ? styles.buttonActive : ""}`} onMouseDown={() => handleMouseDown("A#3")} onMouseEnter={(e) => handleMouseEnter("A#3", e)}></button>
+                <button className={`${styles.key} ${activeButtons.includes("B3") ? styles.buttonActive : ""}`} onMouseDown={() => handleMouseDown("B3")} onMouseEnter={(e) => handleMouseEnter("B3", e)}></button>
+                <button className={`${styles.key} ${activeButtons.includes("C4") ? styles.buttonActive : ""}`} onMouseDown={() => handleMouseDown("C4")} onMouseEnter={(e) => handleMouseEnter("C4", e)}></button>
+                <button className={`${styles.black} ${activeButtons.includes("C#4") ? styles.buttonActive : ""}`} onMouseDown={() => handleMouseDown("C#4")} onMouseEnter={(e) => handleMouseEnter("C#4", e)}></button>
+                <button className={`${styles.key} ${activeButtons.includes("D4") ? styles.buttonActive : ""}`} onMouseDown={() => handleMouseDown("D4")} onMouseEnter={(e) => handleMouseEnter("D4", e)}></button>
+                <button className={`${styles.black} ${activeButtons.includes("D#4") ? styles.buttonActive : ""}`} onMouseDown={() => handleMouseDown("D#4")} onMouseEnter={(e) => handleMouseEnter("D#4", e)}></button>
+                <button className={`${styles.key} ${activeButtons.includes("E4") ? styles.buttonActive : ""}`} onMouseDown={() => handleMouseDown("E4")} onMouseEnter={(e) => handleMouseEnter("E4", e)}></button>
+                <button className={`${styles.key} ${activeButtons.includes("F4") ? styles.buttonActive : ""}`} onMouseDown={() => handleMouseDown("F4")} onMouseEnter={(e) => handleMouseEnter("F4", e)}></button>
+                <button className={`${styles.black} ${activeButtons.includes("F#4") ? styles.buttonActive : ""}`} onMouseDown={() => handleMouseDown("F#4")} onMouseEnter={(e) => handleMouseEnter("F#4", e)}></button>
+                <button className={`${styles.key} ${activeButtons.includes("G4") ? styles.buttonActive : ""}`} onMouseDown={() => handleMouseDown("G4")} onMouseEnter={(e) => handleMouseEnter("G4", e)}></button>
+                <button className={`${styles.black} ${activeButtons.includes("G#4") ? styles.buttonActive : ""}`} onMouseDown={() => handleMouseDown("G#4")} onMouseEnter={(e) => handleMouseEnter("G#4", e)}></button>
+                <button className={`${styles.key} ${activeButtons.includes("A4") ? styles.buttonActive : ""}`} onMouseDown={() => handleMouseDown("A4")} onMouseEnter={(e) => handleMouseEnter("A4", e)}></button>
+                <button className={`${styles.black} ${activeButtons.includes("A#4") ? styles.buttonActive : ""}`} onMouseDown={() => handleMouseDown("A#4")} onMouseEnter={(e) => handleMouseEnter("A#4", e)}></button>
+                <button className={`${styles.key} ${activeButtons.includes("B4") ? styles.buttonActive : ""}`} onMouseDown={() => handleMouseDown("B4")} onMouseEnter={(e) => handleMouseEnter("B4", e)}></button>
+                <button className={`${styles.key} ${activeButtons.includes("C5") ? styles.buttonActive : ""}`} onMouseDown={() => handleMouseDown("C5")} onMouseEnter={(e) => handleMouseEnter("C5", e)}></button>
+                <button className={`${styles.black} ${activeButtons.includes("C#5") ? styles.buttonActive : ""}`} onMouseDown={() => handleMouseDown("C#5")} onMouseEnter={(e) => handleMouseEnter("C#5", e)}></button>
+                <button className={`${styles.key} ${activeButtons.includes("D5") ? styles.buttonActive : ""}`} onMouseDown={() => handleMouseDown("D5")} onMouseEnter={(e) => handleMouseEnter("D5", e)}></button>
+                <button className={`${styles.black} ${activeButtons.includes("D#5") ? styles.buttonActive : ""}`} onMouseDown={() => handleMouseDown("D#5")} onMouseEnter={(e) => handleMouseEnter("D#5", e)}></button>
+                <button className={`${styles.key} ${activeButtons.includes("E5") ? styles.buttonActive : ""}`} onMouseDown={() => handleMouseDown("E5")} onMouseEnter={(e) => handleMouseEnter("E5", e)}></button>
+                <button className={`${styles.key} ${activeButtons.includes("F5") ? styles.buttonActive : ""}`} onMouseDown={() => handleMouseDown("F5")} onMouseEnter={(e) => handleMouseEnter("F5", e)}></button>
+                <button className={`${styles.black} ${activeButtons.includes("F#5") ? styles.buttonActive : ""}`} onMouseDown={() => handleMouseDown("F#5")} onMouseEnter={(e) => handleMouseEnter("F#5", e)}></button>
+                <button className={`${styles.key} ${activeButtons.includes("G5") ? styles.buttonActive : ""}`} onMouseDown={() => handleMouseDown("G5")} onMouseEnter={(e) => handleMouseEnter("G5", e)}></button>
+                <button className={`${styles.black} ${activeButtons.includes("G#5") ? styles.buttonActive : ""}`} onMouseDown={() => handleMouseDown("G#5")} onMouseEnter={(e) => handleMouseEnter("G#5", e)}></button>
+                <button className={`${styles.key} ${activeButtons.includes("A5") ? styles.buttonActive : ""}`} onMouseDown={() => handleMouseDown("A5")} onMouseEnter={(e) => handleMouseEnter("A5", e)}></button>
+                <button className={`${styles.black} ${activeButtons.includes("A#5") ? styles.buttonActive : ""}`} onMouseDown={() => handleMouseDown("A#5")} onMouseEnter={(e) => handleMouseEnter("A#5", e)}></button>
+                <button className={`${styles.key} ${activeButtons.includes("B5") ? styles.buttonActive : ""}`} onMouseDown={() => handleMouseDown("B5")} onMouseEnter={(e) => handleMouseEnter("B5", e)}></button>
             </div>
         </>
     )
